a69cded355ccda6de74f364017c3cab012b49f85
Rahul Sharma <rahul.sharma@siemens-energy.com>
2023-12-27 18:03:28 +0530

    adding error handling middleware and a defined response for errors

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,5 @@
-
-
-using Core.Interfaces;
+using API.Extensions;
+using API.Middleware;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,23 +8,20 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<StoreContext>(options => options.UseSqlServer(builder.Configuration["ConnectionStrings:DefaultConnection"]));
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddApplicationServices(builder.Configuration);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseStatusCodePagesWithReExecute("/errors/{0}");
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseStaticFiles();
+
 app.UseAuthorization();
 
 app.MapControllers();
@@ -41,7 +37,7 @@ try
 }
 catch (Exception ex)
 {
-   logger.LogError(ex, "An error occured during migration");
+    logger.LogError(ex, "An error occured during migration");
 }
 
 app.Run();
